d490a0fb85f85ba76a6ca0210f4a38bd9ff3f99c
Irina <irina@netmagik.com>
2020-09-14 12:20:50 -0400

    Updates

---
+++ precache-manifest.49c49affbd00fbf216751e683b9c72d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef1f0b592fa43d9549d56737fd8f8ad",
+    "url": "/Grocery-List-In-React-CRUD/index.html"
+  },
+  {
+    "revision": "1c22c43788625721d87d",
+    "url": "/Grocery-List-In-React-CRUD/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "204fd38ecbfef489f9ff",
+    "url": "/Grocery-List-In-React-CRUD/static/js/2.98af9e5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-List-In-React-CRUD/static/js/2.98af9e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c22c43788625721d87d",
+    "url": "/Grocery-List-In-React-CRUD/static/js/main.49f43a28.chunk.js"
+  },
+  {
+    "revision": "8f89d9431dd4cbbb6625",
+    "url": "/Grocery-List-In-React-CRUD/static/js/runtime-main.f6680203.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Grocery-List-In-React-CRUD/precache-manifest.49c49affbd00fbf216751e683b9c72d2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Grocery-List-In-React-CRUD/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
